c24079c943230fb25f9f110ff7f792628b240a80
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2023-02-10 15:17:07 -0500

    Use github repos

--- terraform/repositories/repositories.tf
+++ terraform/repositories/repositories.tf
@@ -1,45 +1,60 @@
-resource "ibm_cd_toolchain_tool_hostedgit" "app_repo" {
+resource "ibm_cd_toolchain_tool_githubconsolidated" "app_repo" {
   toolchain_id = var.toolchain_id
-  name         = "repo"
   initialization {
-    type = "clone_if_not_exists"
+    owner_id = var.app_repo_github_user
+    git_id = "github"
+    type = "clone"
     source_repo_url = var.app_repo
     private_repo = true
     repo_name = join("-", [ var.repositories_prefix, "app-repo" ])
+    auto_init = false
   }  
   parameters {
     toolchain_issues_enabled = true
     enable_traceability = true
+    integration_owner = var.app_repo_user_id
+    auth_type = "pat"
+    api_token = var.app_repo_auth_token
   }
 }
 
-resource "ibm_cd_toolchain_tool_hostedgit" "pipeline_repo" {
+resource "ibm_cd_toolchain_tool_githubconsolidated" "pipeline_repo" {
   toolchain_id = var.toolchain_id
-  name         = "pipeline-repo"  
   initialization {
-    type = "clone_if_not_exists"
+    owner_id = var.pipeline_repo_github_user
+    git_id = "github"
+    type = "clone"
     source_repo_url = var.pipeline_repo
     private_repo = true
     repo_name = join("-", [ var.repositories_prefix, "pipeline-repo" ])
+    auto_init = false
   }
   parameters {
     toolchain_issues_enabled = false
     enable_traceability = false
+    integration_owner = var.pipeline_repo_user_id
+    auth_type = "pat"
+    api_token = var.pipeline_repo_auth_token
   }
 }
 
-resource "ibm_cd_toolchain_tool_hostedgit" "tekton_tasks_catalog_repo" {
+resource "ibm_cd_toolchain_tool_githubconsolidated" "tekton_tasks_catalog_repo" {
   toolchain_id = var.toolchain_id
-  name         = "tasks-repo"
   initialization {
-    type = "clone_if_not_exists"
+    owner_id = var.tekton_tasks_catalog_repo_github_user
+    git_id = "github"
+    type = "clone"
     source_repo_url = var.tekton_tasks_catalog_repo
     private_repo = true
     repo_name = join("-", [ var.repositories_prefix, "tasks-repo" ])
+    auto_init = false
   }
   parameters {
+    integration_owner = var.tekton_tasks_catalog_repo_user_id
     toolchain_issues_enabled = false
     enable_traceability = false
+    auth_type = "pat"
+    api_token = var.tekton_tasks_catalog_repo_auth_token
   }
 }
 
